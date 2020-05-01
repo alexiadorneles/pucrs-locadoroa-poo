9e43c6f21e174b7387656ae40f9f9e2a3087aff1
alexiadorneles <lecavsalexia@gmail.com>
2020-05-01 16:37:53 -0300

    adicionar main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import automovel.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Categoria categoria = new Categoria("SUV Médio");
+        Marca marca = new Marca("Jeep");
+        Modelo compass = new ModeloNacional("Compass", 120000, categoria, marca, 25);
+        Modelo cheroke = new ModeloInternacional("Cheroke", 250000, categoria, marca, 75);
+        Automovel automovel = new Automovel("AAA-1A11", 2020, 100, true, compass);
+        Automovel automovelDois = new Automovel("BBB-B2BB", 2020, 200, true, cheroke);
+        System.out.println(automovel.calcularValorFixo());
+        System.out.println(automovelDois.calcularValorFixo());
     }
 }
