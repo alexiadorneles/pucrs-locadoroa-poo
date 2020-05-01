dbc6584ebc1330e9c82c62afbca5dee2dc943364
alexiadorneles <lecavsalexia@gmail.com>
2020-05-01 16:36:05 -0300

    adicionar estrutura automovel

---
+++ src/automovel/Automovel.java
@@ -0,0 +1,41 @@
+package automovel;
+
+public class Automovel {
+    private String placa;
+    private int ano;
+    private double valorDiaria;
+    private boolean disponivel;
+    private Modelo modelo;
+
+    public double calcularValorFixo() {
+        return this.modelo.calcularValorModelo();
+    }
+
+    public Automovel(String placa, int ano, double valorDiaria, boolean disponivel, Modelo modelo) {
+        this.placa = placa;
+        this.ano = ano;
+        this.valorDiaria = valorDiaria;
+        this.disponivel = disponivel;
+        this.modelo = modelo;
+    }
+
+    protected String getPlaca() {
+        return placa;
+    }
+
+    protected int getAno() {
+        return ano;
+    }
+
+    protected double getValorDiaria() {
+        return valorDiaria;
+    }
+
+    protected boolean isDisponivel() {
+        return disponivel;
+    }
+
+    protected Modelo getModelo() {
+        return modelo;
+    }
+}
