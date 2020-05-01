d009fd314604d5950df68a73f226ce0ac9200498
alexiadorneles <lecavsalexia@gmail.com>
2020-05-01 18:18:43 -0300

    adicionar do while

--- src/Menu.java
+++ src/Menu.java
@@ -21,77 +21,87 @@ public class Menu {
 
 
         if (opcao1 == 1) {
-            System.out.println("1 - Cadastrar nomo Cliente");
-            System.out.println("2 - Consultar Disponibilidade de Automóvel por Categoria");
-            System.out.println("3 - Consultar o Valor de uma Locaçao ");
-            System.out.println("4 - Realizar Locação");
-            System.out.println("5 - Finalizar Locação ");
-            System.out.println("99 - Sair ");
+            do {
+                this.mostrarOpcoesAtendente();
+                opcao2 = in.nextInt();
+                switch (opcao2) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            } while (opcao2 != 99);
         } else {
-            System.out.println("1 - Cadastrar Nova Categoria Automóvel");
-            System.out.println("2 - Cadastrar Nova Marca do Automóvel");
-            System.out.println("3 - Cadastrar Novo Modelo do Automóvel");
-            System.out.println("4 - Cadastrar Novo Automóvel");
-            System.out.println("5 - Consultar Locações");
-            System.out.println("6 - Consultar Clientes");
-            System.out.println("7 - Consultar Automóveis Cadastrados");
-            System.out.println("99 - Sair");
-        }
-
-        opcao2 = in.nextInt();
-
-
-        if (opcao1 == 1) {
-            switch (opcao2) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-            }
-        } else {
-            switch (opcao2) {
+            do {
+                this.mostrarOpcoesGerente();
+                opcao2 = in.nextInt();
+                switch (opcao2) {
 //            1 - Cadastrar Nova Categoria Automóvel
-                case 1:
-                    this.cadastrarCategoria(in);
-                    break;
+                    case 1:
+                        this.cadastrarCategoria(in);
+                        break;
 //            2 - Cadastrar Nova Marca do Automóvel
-                case 2:
-                    this.cadastrarMarca(in);
-                    break;
+                    case 2:
+                        this.cadastrarMarca(in);
+                        break;
 //            3 - Cadastrar Novo Modelo do Automóvel
-                case 3:
-                    this.cadastrarModelo(in);
-                    break;
+                    case 3:
+                        this.cadastrarModelo(in);
+                        break;
 //            4 - Cadastrar Novo Automóvel
-                case 4:
-                    System.out.println("Digite a placa: ");
-                    String placa = in.nextLine();
-                    System.out.println("Digite o ano: ");
-                    int ano = in.nextInt();
-                    System.out.println("Digite o valor diária: ");
-                    double valorDiaria = in.nextDouble();
-                    System.out.println("Digite o nome do modelo: ");
-                    String nomeModelo = in.nextLine();
-                    Automovel automovel = new Automovel(
-                            placa, ano, valorDiaria, true,
-                            ModeloRepository.getInstance().findOne(nomeModelo)
-                    );
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-            }
+                    case 4:
+                        this.cadastrarAutomovel(in);
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                }
+            } while (opcao2 != 99);
         }
+    }
 
+    private void cadastrarAutomovel(Scanner in) {
+        System.out.println("Digite a placa: ");
+        String placa = in.nextLine();
+        System.out.println("Digite o ano: ");
+        int ano = in.nextInt();
+        System.out.println("Digite o valor diária: ");
+        double valorDiaria = in.nextDouble();
+        System.out.println("Digite o nome do modelo: ");
+        String nomeModelo = in.nextLine();
+        Automovel automovel = new Automovel(
+                placa, ano, valorDiaria, true,
+                ModeloRepository.getInstance().findOne(nomeModelo)
+        );
+    }
+
+    private void mostrarOpcoesGerente() {
+        System.out.println("1 - Cadastrar Nova Categoria Automóvel");
+        System.out.println("2 - Cadastrar Nova Marca do Automóvel");
+        System.out.println("3 - Cadastrar Novo Modelo do Automóvel");
+        System.out.println("4 - Cadastrar Novo Automóvel");
+        System.out.println("5 - Consultar Locações");
+        System.out.println("6 - Consultar Clientes");
+        System.out.println("7 - Consultar Automóveis Cadastrados");
+        System.out.println("99 - Sair");
+    }
+
+    private void mostrarOpcoesAtendente() {
+        System.out.println("1 - Cadastrar nomo Cliente");
+        System.out.println("2 - Consultar Disponibilidade de Automóvel por Categoria");
+        System.out.println("3 - Consultar o Valor de uma Locaçao ");
+        System.out.println("4 - Realizar Locação");
+        System.out.println("5 - Finalizar Locação ");
+        System.out.println("99 - Sair ");
     }
 
     private void cadastrarCategoria(Scanner in) {
