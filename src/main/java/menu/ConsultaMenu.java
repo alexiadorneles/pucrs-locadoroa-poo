299548069ed7b1611f0eaddf83b52510d6ca6775
Aléxia <lecavsalexia@gmail.com>
2020-07-06 01:16:23 -0300

    fix consulta cliente.

--- src/main/java/menu/ConsultaMenu.java
+++ src/main/java/menu/ConsultaMenu.java
@@ -1,5 +1,5 @@
 package menu;
-import domain.cliente.PessoaFisica;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -7,7 +7,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -23,11 +22,10 @@ import domain.locacao.Locacao;
 import repository.*;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class ConsultaMenu extends Application{
-    public int button=0;
+public class ConsultaMenu extends Application {
+    public int button = 0;
     private final Repository<Automovel, String> automovelRepository;
     private final Repository<Locacao, Integer> locacaoRepository;
 
@@ -36,8 +34,8 @@ public class ConsultaMenu extends Application{
         this.locacaoRepository = locacaoRepository;
     }
 
-    public void setButton(int i){
-        if(i>0 && i<15) button=i;
+    public void setButton(int i) {
+        if (i > 0 && i < 15) button = i;
         else button = 0;
     }
 
@@ -50,32 +48,32 @@ public class ConsultaMenu extends Application{
         grid.setVgap(10);
         grid.setPadding(new Insets(100, 100, 100, 100));
 
-        switch (button){
+        switch (button) {
             case 2:
                 Text title = new Text("CONSULTA DISPONIBILIDADE DE AUTOMOVEL POR CATEGORIA");
-                title.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
+                title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                 title.setTextAlignment(TextAlignment.CENTER);
-                grid.add(title,0,0);
+                grid.add(title, 0, 0);
 
                 Label text = new Label("Codigo (números): ");
-                grid.add(text,0,1);
+                grid.add(text, 0, 1);
 
                 TextField categoria = new TextField();
-                grid.add(categoria,1,1);
+                grid.add(categoria, 1, 1);
 
 
                 Button verificar = new Button("VERIFICAR");
                 HBox button1 = new HBox(10);
                 button1.setAlignment(Pos.BOTTOM_LEFT);
                 button1.getChildren().add(verificar);
-                grid.add(button1,1,2);
+                grid.add(button1, 1, 2);
 
                 final Text actiontarget = new Text();
                 grid.add(actiontarget, 1, 6);
                 actiontarget.setId("actiontarget");
 
 
-                verificar.setOnAction(new EventHandler<ActionEvent>()  {
+                verificar.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent actionEvent) {
                         actiontarget.setFill(Color.FIREBRICK);
@@ -89,16 +87,16 @@ public class ConsultaMenu extends Application{
                             }
 
                             Text categoriaDisponivel = new Text("Automoveis disponiveis");
-                            categoriaDisponivel.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
+                            categoriaDisponivel.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                             categoriaDisponivel.setTextAlignment(TextAlignment.CENTER);
-                            grid.add(categoriaDisponivel,0,5);
+                            grid.add(categoriaDisponivel, 0, 5);
                             Text txt = new Text();
-                            txt.setFont(Font.font("Tahoma",FontWeight.NORMAL,14));
+                            txt.setFont(Font.font("Tahoma", FontWeight.NORMAL, 14));
                             autoDisponiveisCategoria.forEach(str -> txt.setText(str.toString()));
-                            grid.add(txt,0,6);
+                            grid.add(txt, 0, 6);
                         }
                     }
-                }) ;
+                });
                 Scene scene = new Scene(grid);
                 menuConsulta.setScene(scene);
                 menuConsulta.show();
@@ -106,38 +104,38 @@ public class ConsultaMenu extends Application{
                 break;
             case 3:
                 Text title2 = new Text("CONSULTAR O VALOR DE UMA LOCAÇÃO");
-                title2.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
+                title2.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                 title2.setTextAlignment(TextAlignment.CENTER);
-                grid.add(title2,0,0);
+                grid.add(title2, 0, 0);
 
                 Text locacaoDisponivel = new Text("Locações disponiveis");
-                locacaoDisponivel.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
+                locacaoDisponivel.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                 locacaoDisponivel.setTextAlignment(TextAlignment.CENTER);
-                grid.add(locacaoDisponivel,0,5);
+                grid.add(locacaoDisponivel, 0, 5);
                 Text locacaod = new Text("");
-                locacaod.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
+                locacaod.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                 locacaoRepository.findAll().forEach(str -> locacaod.setText(str.toString()));
-                grid.add(locacaod,0,6);
+                grid.add(locacaod, 0, 6);
 
                 Label text2 = new Label("Codigo: ");
-                grid.add(text2,0,1);
+                grid.add(text2, 0, 1);
 
                 TextField codigo = new TextField();
-                grid.add(codigo,1,1);
+                grid.add(codigo, 1, 1);
 
 
                 Button consult = new Button("VERIFICAR");
                 HBox button2 = new HBox(10);
                 button2.setAlignment(Pos.BOTTOM_LEFT);
                 button2.getChildren().add(consult);
-                grid.add(button2,1,2);
+                grid.add(button2, 1, 2);
 
                 final Text action = new Text();
                 grid.add(action, 1, 6);
                 action.setId("action");
 
 
-                consult.setOnAction(new EventHandler<ActionEvent>()  {
+                consult.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent actionEvent) {
                         action.setFill(Color.FIREBRICK);
@@ -147,16 +145,16 @@ public class ConsultaMenu extends Application{
                             locacao = locacaoRepository.findOne(Integer.valueOf(codigo.getText()));
                             try {
                                 Text t2 = new Text("O valor da locação é " + locacao.calcularValorLocacao());
-                                t2.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
-                                grid.add(t2,0,2);
+                                t2.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+                                grid.add(t2, 0, 2);
                             } catch (Exception e) {
                                 Text t2 = new Text("Houve um problema ao calcular o valor da locação. Certifique-se que ela esteja ligada a um automóvel.");
-                                t2.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
-                                grid.add(t2,0,2);
+                                t2.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+                                grid.add(t2, 0, 2);
                             }
                         }
                     }
-                }) ;
+                });
                 Scene scene2 = new Scene(grid);
                 menuConsulta.setScene(scene2);
                 menuConsulta.show();
@@ -164,54 +162,57 @@ public class ConsultaMenu extends Application{
                 break;
             case 4:
                 Text title3 = new Text("CONSULTAR LOCACOES");
-                title3.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
+                title3.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                 title3.setTextAlignment(TextAlignment.CENTER);
-                grid.add(title3,0,0);
+                grid.add(title3, 0, 0);
 
                 Label text1 = new Label("Locações disponiveis");
-                grid.add(text1,0,1);
+                grid.add(text1, 0, 1);
 
                 Text action1 = new Text();
                 action1.setId("action");
                 action1.setFill(Color.FIREBRICK);
-                grid.add(action1,0,6);
+                grid.add(action1, 0, 6);
 
                 List<Locacao> locacoes = locacaoRepository.filter(locacao -> !locacao.isFinalizada());
                 if (locacoes.isEmpty()) {
                     action1.setText("Nenhuma locação disponivel");
-                }else {
+                } else {
                     Text loc = new Text();
-                    loc.setFont(Font.font("Tahoma",FontWeight.NORMAL,14));
+                    loc.setFont(Font.font("Tahoma", FontWeight.NORMAL, 14));
                     loc.setTextAlignment(TextAlignment.CENTER);
-                    locacoes.forEach(str-> loc.setText(str.toString()));
-                    grid.add(loc,0,5);
+                    locacoes.forEach(str -> loc.setText(str.toString()));
+                    grid.add(loc, 0, 5);
                 }
                 menuConsulta.setScene(new Scene(grid));
                 menuConsulta.show();
                 break;
             case 5:
                 Text title4 = new Text("CONSULTAR CLIENTES");
-                title4.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
+                title4.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                 title4.setTextAlignment(TextAlignment.CENTER);
-                grid.add(title4,0,0);
+                grid.add(title4, 0, 0);
 
                 Label text3 = new Label("Clientes disponiveis");
-                grid.add(text3,0,1);
+                grid.add(text3, 0, 1);
 
                 Text action2 = new Text();
                 action2.setId("action");
                 action2.setFill(Color.FIREBRICK);
-                grid.add(action2,0,6);
+                grid.add(action2, 0, 6);
 
                 List<Cliente> clientes = ClienteRepository.getInstance().findAll();
                 if (clientes.isEmpty()) {
                     action2.setText("Nenhum cliente disponivel");
-                }else {
-                    Text cli = new Text();
-                    cli.setFont(Font.font("Tahoma",FontWeight.NORMAL,14));
-                    cli.setTextAlignment(TextAlignment.CENTER);
-                    clientes.forEach(str-> cli.setText(str.toString()));
-                    grid.add(cli,0,5);
+                } else {
+                    AtomicInteger count = new AtomicInteger(5);
+                    clientes.forEach(cliente -> {
+                        Text cli = new Text();
+                        cli.setFont(Font.font("Tahoma", FontWeight.NORMAL, 14));
+                        cli.setTextAlignment(TextAlignment.CENTER);
+                        cli.setText(cliente.toString() + " - " + cliente.getCPFCNPJ());
+                        grid.add(cli, 0, count.incrementAndGet());
+                    });
                 }
                 menuConsulta.setScene(new Scene(grid));
                 menuConsulta.show();
@@ -219,34 +220,35 @@ public class ConsultaMenu extends Application{
 
             case 6:
                 Text title5 = new Text("CONSULTAR AUTOMOVEIS");
-                title5.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
+                title5.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
                 title5.setTextAlignment(TextAlignment.CENTER);
-                grid.add(title5,0,0);
+                grid.add(title5, 0, 0);
 
                 Label text4 = new Label("Automoveis cadastrados");
-                grid.add(text4,0,1);
+                grid.add(text4, 0, 1);
 
                 Text action3 = new Text();
                 action3.setId("action");
                 action3.setFill(Color.FIREBRICK);
-                grid.add(action3,0,6);
+                grid.add(action3, 0, 6);
 
                 List<Automovel> automoveis = this.automovelRepository.findAll();
                 if (automoveis.isEmpty()) {
                     action3.setText("Nenhum automovel cadastrado");
-                }else {
+                } else {
                     Text auto = new Text();
-                    auto.setFont(Font.font("Tahoma",FontWeight.NORMAL,14));
+                    auto.setFont(Font.font("Tahoma", FontWeight.NORMAL, 14));
                     auto.setTextAlignment(TextAlignment.CENTER);
-                    automoveis.forEach(str-> auto.setText(str.toString()));
-                    grid.add(auto,0,5);
+                    automoveis.forEach(str -> auto.setText(str.toString()));
+                    grid.add(auto, 0, 5);
                 }
                 menuConsulta.setScene(new Scene(grid));
                 menuConsulta.show();
                 break;
         }
     }
-    public boolean consultaDisponibilidadeCategoria(String codigo){
+
+    public boolean consultaDisponibilidadeCategoria(String codigo) {
         Categoria categoria = CategoriaRepository.getInstance().findOne(Integer.valueOf(codigo));
         List<Automovel> autoDisponiveisCategoria = this.automovelRepository.filter(auto -> this.getAutomovelByCategoriaAndDisponivel(categoria, auto));
         if (autoDisponiveisCategoria.isEmpty()) {
