5fa7b80b4bb60166a2b1a204094a707e8708743e
Aléxia <lecavsalexia@gmail.com>
2020-07-05 22:25:31 -0300

    open domain.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example {
     requires javafx.controls;
     requires com.google.gson;
     requires java.desktop;
+    opens domain to com.google.gson;
     exports domain;
     exports menu;
 }
